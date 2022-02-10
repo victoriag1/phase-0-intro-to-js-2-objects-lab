cec6e88758e80e19ac4aeef3cb112d00f5655b31
Victoria Gonzalez <gonzalezevic@gmail.com>
2022-02-09 17:23:35 -0800

    commit tree 4326fbb95b71a9a2c21af9a73fe9f32b543d609d

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-const employee = { 
-    name: "Victoria", 
-    streetAdress: "11 Broadway", 
-    } 
-
+const employee = {
+    name: "Victoria",
+    streetAdress: "11 Broadway",
+    }
+  
     function updateEmployeeWithKeyAndValue(employee, key, value) {
         return Object.assign({},employee, {[key]: value})
     }
     function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
-        employee[key] = value 
+        employee[key] = value
         return employee
     }
     function deleteFromEmployeeByKey(employee, key) {
         const newObject = Object.assign ({}, employee)
         delete newObject[key]
-        return newObject 
+        return newObject
     }
     function destructivelyDeleteFromEmployeeByKey(employee, key) {
-        delete employee[key] 
-        return employee; 
+        delete employee[key]
+        return employee;
     }
